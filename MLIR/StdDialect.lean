0001519b74d35297be5cc97f96586dae88db7e4f
Siddharth Bhat <siddu.druid@gmail.com>
2021-10-09 15:53:54 +0530

    make everything live in mlir_op syntax category so we can compose them

--- MLIR/StdDialect.lean
+++ MLIR/StdDialect.lean
@@ -8,18 +8,18 @@ open MLIR.EDSL
 
 syntax "addi" mlir_op_operand mlir_op_operand : mlir_op
 
-syntax "br" mlir_op_successor_arg : term
-syntax "cond_br" mlir_op_operand "," mlir_op_successor_arg "," mlir_op_successor_arg : term
+syntax "br" mlir_op_successor_arg : mlir_op
+syntax "cond_br" mlir_op_operand "," mlir_op_successor_arg "," mlir_op_successor_arg : mlir_op
 
 macro_rules
   | `(mlir_op% addi $op1:mlir_op_operand $op2:mlir_op_operand ) => 
         `(mlir_op% "std.addi" ($op1, $op2) : (i 32, i 32) )
 macro_rules
-  | `(br $op1:mlir_op_successor_arg) => 
+  | `(mlir_op% br $op1:mlir_op_successor_arg) => 
         `(mlir_op% "br" () [$op1] : () -> ())
 
 macro_rules
-  | `(cond_br $flag: mlir_op_operand ,
+  | `(mlir_op% cond_br $flag: mlir_op_operand ,
           $truebb:mlir_op_successor_arg , 
           $falsebb:mlir_op_successor_arg) => 
         `(mlir_op% "cond_br" ($flag) [$truebb, $falsebb] : (i 1) -> () )
@@ -30,21 +30,22 @@ macro_rules
 def add0 : Op := (mlir_op% addi %c0 %c1)
 #print add0
 
-def br0 : Op := br ^entry
+def br0 : Op := (mlir_op% br ^entry)
 #print br0
 
-def condbr0 : Op := cond_br %flag, ^loopheader, ^loopexit
+def condbr0 : Op := (mlir_op% cond_br %flag, ^loopheader, ^loopexit)
 #print condbr0
 
 
-syntax "while" "(" mlir_op_operand ")" ":" mlir_type mlir_region : term
+syntax "while" "(" mlir_op_operand ")" ":" mlir_type mlir_region : mlir_op
+
 
 macro_rules
-  | `(while ( $flag ) : $retty  $body ) => 
+  | `(mlir_op% while ( $flag ) : $retty  $body ) => 
         `(mlir_op% "while" ($flag) ($body) : $retty )
 
-def while0 := while (%x) : (i 32) -> (i 32) { 
+def while0 := (mlir_op% while (%x) : (i 32) -> (i 32) { 
     ^entry: 
       addi %c0 %x
-}
+})
 #print while0
