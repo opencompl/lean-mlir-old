81fbc071ebce499d2582feee2da88935e7611f7b
Siddharth Bhat <siddu.druid@gmail.com>
2021-10-09 15:48:28 +0530

    figure out how to get custom ops working

--- MLIR/StdDialect.lean
+++ MLIR/StdDialect.lean
@@ -34,3 +34,16 @@ def br0 : Op := br ^entry
 
 def condbr0 : Op := cond_br %flag, ^loopheader, ^loopexit
 #print condbr0
+
+
+syntax "while" "(" mlir_op_operand ")" ":" mlir_type mlir_region : term
+
+macro_rules
+  | `(while ( $flag ) : $retty  $body ) => 
+        `(mlir_op% "while" ($flag) ($body) : $retty )
+
+def while0 := while (%x) : (i 32) -> (i 32) { 
+    ^entry: 
+      -- addi %c0 %x
+}
+#print while0
