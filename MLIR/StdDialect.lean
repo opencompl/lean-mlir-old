cfb5cc3f619f1406974fead4949606ac94218244
Siddharth Bhat <siddu.druid@gmail.com>
2021-10-09 13:50:21 +0530

    add std.addi

--- MLIR/StdDialect.lean
+++ MLIR/StdDialect.lean
@@ -4,4 +4,11 @@ import MLIR.EDSL
 open MLIR.AST
 open MLIR.EDSL
 
--- def addi (x: SSAVal) (y: SSAVal): SSAVal := (mlir_op_call% "std.addi" (x, y) : (i 32, i 32) -> (i 32) )
+
+syntax "addi" mlir_op_operand mlir_op_operand : term
+
+macro_rules
+  | `(addi $op1:mlir_op_operand $op2:mlir_op_operand ) => `(mlir_op_call% "std.addi"($op1, $op2) : (i 32, i 32) )
+
+def add0 : Op := addi %c0 %c1
+#print add0
