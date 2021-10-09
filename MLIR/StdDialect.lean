a809991a2b04bd529274280c68f26bef83276df2
Siddharth Bhat <siddu.druid@gmail.com>
2021-10-09 15:51:07 +0530

    add while loop example

--- MLIR/StdDialect.lean
+++ MLIR/StdDialect.lean
@@ -6,12 +6,13 @@ open MLIR.EDSL
 
 -- https://mlir.llvm.org/docs/Dialects/Standard/        
 
-syntax "addi" mlir_op_operand mlir_op_operand : term
+syntax "addi" mlir_op_operand mlir_op_operand : mlir_op
+
 syntax "br" mlir_op_successor_arg : term
 syntax "cond_br" mlir_op_operand "," mlir_op_successor_arg "," mlir_op_successor_arg : term
 
 macro_rules
-  | `(addi $op1:mlir_op_operand $op2:mlir_op_operand ) => 
+  | `(mlir_op% addi $op1:mlir_op_operand $op2:mlir_op_operand ) => 
         `(mlir_op% "std.addi" ($op1, $op2) : (i 32, i 32) )
 macro_rules
   | `(br $op1:mlir_op_successor_arg) => 
@@ -26,7 +27,7 @@ macro_rules
 -- | TODO: add block arguments. 
 -- syntax "br" 
 
-def add0 : Op := addi %c0 %c1
+def add0 : Op := (mlir_op% addi %c0 %c1)
 #print add0
 
 def br0 : Op := br ^entry
@@ -44,6 +45,6 @@ macro_rules
 
 def while0 := while (%x) : (i 32) -> (i 32) { 
     ^entry: 
-      -- addi %c0 %x
+      addi %c0 %x
 }
 #print while0
