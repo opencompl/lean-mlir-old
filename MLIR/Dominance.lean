2543a272b2e79f28f7b7132f0ebeeffe5b4e2843
Siddharth Bhat <siddu.druid@gmail.com>
2022-02-16 11:13:55 +0000

    add dominance and LICENSE

---
+++ MLIR/Dominance.lean
@@ -0,0 +1,12 @@
+/-
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Anurudh Peduri
+-/
+
+
+import MLIR.AST
+
+-- | define what it means for SSA dominance to hold, and 
+-- | write a checker that decidably checks whether SSA condition
+-- | holds in the region.
+theorem region_obeys_ssa (r: Region): bool := sorry
