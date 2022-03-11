03095b19b962727141894fc2747d421af1268718
Sébastien Michelland <sebastien.michelland@ens-lyon.fr>
2022-03-11 09:32:37 +0000

    meta: import-oriented directory structure

--- Main.lean
+++
@@ -1,5 +0,0 @@
-import MLIRSemantics
-import MLIR.MLIRParser
-
-def main : IO Unit :=
-  IO.println s!"Hello"
--- lakefile.lean
+++ lakefile.lean
@@ -2,6 +2,7 @@ import Lake
 open Lake DSL
 
 package MLIRSemantics {
+  binRoot := `MLIRSemantics
   dependencies := #[{
     name := "MLIR",
     src := Source.git "https://github.com/opencompl/lean-mlir.git" "e29d0850e943198d50b691aeed7b6eec0be202df",
