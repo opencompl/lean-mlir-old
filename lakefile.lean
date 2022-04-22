dec53993468d8f21cdd1be56b8975d01d8678ba4
Sébastien Michelland <sebastien.michelland@ens-lyon.fr>
2022-04-22 13:57:18 +0100

    meta: update to latest lean-mlir and more recent Lean nightly [WIP]

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,6 @@ package MLIRSemantics {
   binRoot := `MLIRSemantics
   dependencies := #[{
     name := "MLIR",
-    src := Source.git "https://github.com/lephe/lean-mlir.git" "3a3d71390b1dff65606e2e167d1cdc01d8304397",
+    src := Source.git "https://github.com/lephe/lean-mlir.git" "5d29c1c1494e189cc1e519875534f52749fc1f5a",
   }]
 }
