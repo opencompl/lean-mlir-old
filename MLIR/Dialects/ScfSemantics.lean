83f8ac467e15b94ec17b8f9894dc644ac082f32f
Siddharth Bhat <siddu.druid@gmail.com>
2022-06-05 13:31:19 +0100

    Add Scf.For
    There's a `sorry` in there that I'm too lazy to fill out
    about FITree reshaping. Will do this later.

--- MLIR/Dialects/ScfSemantics.lean
+++ MLIR/Dialects/ScfSemantics.lean
@@ -17,8 +17,8 @@ instance scf: Dialect Void Void (fun x => Unit) where
 
 -- | interesting, what is the type of ScfFor?
 -- | TODO: use the return type. For now, just do unit.
-inductive ScfE: Type → Type :=
-  | For: (low:Int) → (upper: Int) → (step: Int)  → ScfE Unit
+inductive ScfE (δ: Dialect α σ ε): Type → Type :=
+  | For: (low:Int) → (upper: Int) → (step: Int)  → (r: Region δ) → ScfE δ Unit
 
 
 
@@ -43,13 +43,13 @@ def run_loop_bounded [Monad m] (n: Nat) (lo: Int) (step: Int) (accum: a) (eff: I
 -- | TODO: refactor to (1) an effect, (2) an interpretation
 def scf_semantics_op {Gα Gσ Gε} {Gδ: Dialect Gα Gσ Gε} [S: Semantics Gδ]
   (ret_name: Option SSAVal):
-        Op Gδ → Option (Fitree (UBE +' SSAEnvE Gδ +' S.E Gδ +' ScfE) (BlockResult Gδ))
+        Op Gδ → Option (Fitree (UBE +' SSAEnvE Gδ +' S.E Gδ +' ScfE Gδ) (BlockResult Gδ))
   | Op.mk "scf.for" [lo, hi, step] _ [r] _ (.fn (.tuple []) (.int sgn sz)) => some do
       let lo : FinInt sz <- SSAEnv.get? Gδ (MLIRType.int sgn sz) lo
       let hi : FinInt sz <- SSAEnv.get? Gδ (MLIRType.int sgn sz) hi
       let step : FinInt sz <- SSAEnv.get? Gδ (MLIRType.int sgn sz) step
-      -- let rsem := semantics_region_single_bb r
-      let t <- Fitree.trigger (ScfE.For (FinInt.toSint' lo) (FinInt.toSint' hi) (FinInt.toSint' step));
+      let rsem := semantics_region_single_bb r
+      let t <- Fitree.trigger (ScfE.For (δ := Gδ) (FinInt.toSint' lo) (FinInt.toSint' hi) (FinInt.toSint' step) r);
       SSAEnv.set? (δ := Gδ) MLIRType.unit ret_name ()
       -- let nsteps : Int := ((FinInt.toSint'  hi) - (FinInt.toSint' lo)) / FinInt.toSint' step
       -- let out <- run_loop_bounded (a := PUnit) 
@@ -64,76 +64,35 @@ def scf_semantics_op {Gα Gσ Gε} {Gδ: Dialect Gα Gσ Gε} [S: Semantics Gδ]
       return BlockResult.Next
   | _ => none
 
-def handle_scf {E}: ScfE ~> Fitree E :=
+private def eff_inject {E} [Semantics δ] (x: Fitree (UBE +' SSAEnvE δ +' Semantics.E δ) Unit): 
+    Fitree (UBE +' SSAEnvE δ +' Semantics.E δ +' E) PUnit := 
+  -- TODO: fill up sorry for translation
+  let y: Fitree (UBE +' SSAEnvE δ +' Semantics.E δ +' E) Unit := Fitree.translate sorry x
+  let z : Fitree (UBE +' SSAEnvE δ +' Semantics.E δ +' E) PUnit := 
+    Functor.map (fun _ => PUnit.unit) y
+  z
+
+
+def handle_scf {E} [Semantics δ]: ScfE δ ~> Fitree  (UBE +' SSAEnvE δ +' Semantics.E δ +' E)  :=
   fun _ e =>
     match e with
-    | .For lo hi step => do
+    | .For lo hi step r => do
       let nsteps : Int := (hi - lo) / step
       let out <- run_loop_bounded (a := PUnit) 
                  (n := nsteps.toNat)
                  (lo := lo)
                  (step := step)
                  (accum := PUnit.unit)
-                 (eff := (fun i _ => (semantics_region_single_bb r))) -- how to type this correctly?
-                 -- (eff := (fun i _ => pure PUnit.unit))
-
-
-instance: Semantics sef where
-  E := ScfE
+                 (eff := (fun i _ => eff_inject (semantics_region_single_bb (Gδ := δ) r)))
+       return ()
+/-
+instance  : Semantics scf where
+  E := ScfE δ
   semantics_op := scf_semantics_op
   handle := handle_scf
+-/
 
 /-
 ### Examples and testing
 -/
 
--- #reduce spins on the dialect coercion because it's mutually inductive, even
--- with skipProofs := true (why?!), so define it directly in the dummy dialect
-def dummy_stmt: BasicBlockStmt dummy := [mlir_bb_stmt|
-  %dummy = "dummy.dummy"() : () -> i32
-]
-
-def true_stmt: BasicBlockStmt dummy := [mlir_bb_stmt|
-  %true = "dummy.true"() : () -> i1
-]
-
-def false_stmt: BasicBlockStmt dummy := [mlir_bb_stmt|
-  %false = "dummy.false"() : () -> i1
-]
-
-def run_dummy_cf_region: Region (dummy + cf) → String := fun r =>
-  runLogged (semantics_region 99 r) SSAEnv.empty |>.fst |>.snd
-
-def ex_branch_true: Region dummy := [mlir_region| {
-  ^entry:
-    %x = "dummy.true"() : () -> i1
-    "cf.condbr"(%x) [^bbtrue, ^bbfalse] : ()
-
-  ^bbtrue:
-    %y = "dummy.dummy" () : () -> i32
-    "cf.ret" () : () -> ()
-
-  ^bbfalse:
-    %z = "dummy.dummy" () : () -> i32
-    "cf.ret" () : () -> ()
-}]
-
-#eval ex_branch_true
-#eval run_dummy_cf_region ex_branch_true
-
-def ex_branch_false := [mlir_region| {
-  ^entry:
-    %x = "dummy.false"() : () -> i1
-    "cf.condbr"(%x) [^bbtrue, ^bbfalse] : ()
-
-  ^bbtrue:
-    %y = "dummy.dummy" () : () -> i32
-    "cf.ret" () : () -> ()
-
-  ^bbfalse:
-    %z = "dummy.dummy" () : () -> i32
-    "cf.ret" () : () -> ()
-}]
-
-#eval ex_branch_false
-#eval run_dummy_cf_region ex_branch_false
