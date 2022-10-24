250b5f3457c7f4d9c17f9c79063b5dfc18fb6be8
Siddharth Bhat <siddu.druid@gmail.com>
2022-10-24 16:13:36 +0100

    add dependency on mathlib4,std4,quote4

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,13 @@ package «MLIR»
 
 lean_lib MLIR
 
-@[defaultTarget]
+@[default_target]
 lean_exe «mlir» {
   root := `MLIR
   supportInterpreter := true
 }
 
-require «egg-tactic» from git  "https://github.com/opencompl/egg-tactic-code" @ "499ef2d"
+require «egg-tactic» from git  "https://github.com/opencompl/egg-tactic-code" @ "a493294"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "e094621836b13a341f006ca926e4e24ed09a9c1c"
+require std from git "https://github.com/leanprover/std4.git" @ "d1d14d616938a07210b11d78165421a9a9163e48"
+require Qq from git "https://github.com/gebner/quote4.git" @ "5be427045b61c9d40f9f4b46b3ade98e019a1ad1"
