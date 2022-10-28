2992e2b125c3cde0a1a6de34f337c8e6183ab5c9
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-10-28 10:36:34 +0100

    Add equiv to SSAEnv, improve and add lemmas

--- MLIR/Semantics/SSAEnv.lean
+++ MLIR/Semantics/SSAEnv.lean
@@ -72,6 +72,10 @@ def SSAScope.set {δ: Dialect α σ ε} (name: SSAVal) (τ: MLIRType δ) (v: τ.
 def SSAScope.str {δ: Dialect α σ ε} (scope: SSAScope δ): String :=
   "\n".intercalate <| scope.map fun ⟨name, τ, v⟩ => s!"{name} = {v} : {τ}"
 
+-- Leibniz-ish equality
+def SSAScope.equiv {δ: Dialect α σ ε} (scope1 scope2: SSAScope δ): Prop :=
+  ∀ name τ, scope1.get name τ = scope2.get name τ
+
 instance {δ: Dialect α σ ε}: ToString (SSAScope δ) where
   toString := SSAScope.str
 
@@ -85,11 +89,90 @@ def SSAScope.free (name: SSAVal) (l: SSAScope): Bool :=
 def SSAScope.maps (l: SSAScope) (name: SSAVal) (τ: MLIRTy) (v: τ.eval) :=
   l.Mem ⟨name, τ, v⟩ -/
 
--- SSAScope proofs
+/-
+### SSAScope proofs
+-/
 
-theorem SSAScope.getT_set_ne (v v': SSAVal):
+theorem SSAScope.equiv_refl {δ: Dialect α σ ε} (scope: SSAScope δ):
+  scope.equiv scope := by intros name τ; rfl
+
+theorem SSAScope.equiv_symm {δ: Dialect α σ ε} ⦃scope scope': SSAScope δ⦄:
+    scope.equiv scope' → scope'.equiv scope := by
+  intros H name τ
+  specialize H name τ
+  simp [H]
+
+theorem SSAScope.equiv_trans {δ: Dialect α σ ε} ⦃scope₁ scope₂: SSAScope δ⦄:
+    scope₁.equiv scope₂ →
+    ∀ ⦃scope₃⦄, scope₂.equiv scope₃ →
+    scope₁.equiv scope₃ := by
+  intros H1 scope₃ H2 name τ
+  specialize H1 name τ
+  specialize H2 name τ
+  simp [H1, H2]
+
+theorem SSAScope.get_to_getT {δ: Dialect α σ ε} (name: SSAVal)
+    (scope: SSAScope δ) (τ: MLIRType δ):
+  scope.get name τ =
+    match scope.getT name with
+    | none => none
+    | some ⟨τ', v'⟩ =>
+      if H: τ' = τ then
+        some (cast (by simp [H]) v')
+      else
+        none := by
+  induction scope
+  case nil => simp
+  case cons head tail HInd =>
+  unfold getT get
+  byCases Hname: head.fst = name
+  case h2 =>
+    rw [HInd]
+
+theorem SSAScope.get_some_getT {δ: Dialect α σ ε} ⦃name: SSAVal⦄
+  ⦃scope: SSAScope δ⦄ ⦃τ: MLIRType δ⦄ ⦃v⦄:
+    scope.get name τ = some v →
+    scope.getT name = some ⟨τ, v⟩ := by
+  rw [get_to_getT]
+  split <;> simp at * <;> try contradiction
+  case h_2 _ τ' _ Hget =>
+  byCases Hτ: τ' = τ
+  intros H
+  rw [←H, Hget]
+  rfl
+
+theorem SSAScope.getT_none_get {δ: Dialect α σ ε} ⦃name: SSAVal⦄
+  ⦃scope: SSAScope δ⦄:
+    scope.getT name = none →
+    ∀ τ, scope.get name τ = none := by
+  induction scope <;> simp
+  case cons head tail HInd =>
+  byCases Hname: head.fst = name <;> assumption
+
+theorem SSAScope.getT_some_get {δ: Dialect α σ ε} ⦃name: SSAVal⦄
+  ⦃scope: SSAScope δ⦄ ⦃τ v⦄:
+    scope.getT name = some ⟨τ, v⟩ →
+    scope.get name τ = some v := by
+  induction scope <;> simp
+  case cons head tail HInd =>
+  byCases Hname: head.fst = name <;> try assumption
+  have ⟨headName, headτ, headVal⟩ := head; simp at *
+  intros H; cases H
+  simp; rfl
+
+theorem SSAScope.get_none_getT {δ: Dialect α σ ε} ⦃name: SSAVal⦄
+  ⦃scope: SSAScope δ⦄:
+    (∀ τ, scope.get name τ = none) →
+    scope.getT name = none := by
+  induction scope <;> simp
+  case cons head tail HInd =>
+  byCases Hname: head.fst = name <;> try assumption
+  intros H; specialize H head.2.fst
+  simp at H
+
+theorem SSAScope.getT_set_ne ⦃v v': SSAVal⦄:
     v' ≠ v →
-    ∀ (scope: SSAScope δ) (τ: MLIRType δ) val,
+    ∀ ⦃scope: SSAScope δ⦄ ⦃τ: MLIRType δ⦄ ⦃val⦄,
     getT v (set v' τ val scope) = getT v scope := by
   intros Hne scope τ val
   induction scope with
@@ -110,9 +193,9 @@ theorem SSAScope.getT_set_eq (scope: SSAScope δ) (v: SSAVal) (τ: MLIRType δ) 
     byCases H: head.fst = v
     assumption
 
-theorem SSAScope.get_set_ne_val (v v': SSAVal):
+theorem SSAScope.get_set_ne_val ⦃v v': SSAVal⦄:
     v' ≠ v →
-    ∀ (scope: SSAScope δ) (τ τ': MLIRType δ) val,
+    ∀ ⦃scope: SSAScope δ⦄ ⦃τ τ' val⦄,
     get v (set v' τ val scope) τ' = get v scope τ' := by
   intros Hne scope τ τ' val
   induction scope with
@@ -123,9 +206,9 @@ theorem SSAScope.get_set_ne_val (v v': SSAVal):
     . simp [Hne]
     . byCases H2: head.fst = v <;> try assumption
 
-theorem SSAScope.get_set_ne_type (τ τ': MLIRType δ):
+theorem SSAScope.get_set_ne_type ⦃τ τ': MLIRType δ⦄:
     τ' ≠ τ →
-    ∀ (scope: SSAScope δ) (v: SSAVal) val,
+    ∀ ⦃scope: SSAScope δ⦄ ⦃v: SSAVal⦄ ⦃val⦄,
     get v (set v τ' val scope) τ = none := by
   intros Hne scope v val
   induction scope with
@@ -145,7 +228,28 @@ theorem SSAScope.get_set_eq (v: SSAVal) (scope: SSAScope δ) (τ: MLIRType δ) v
     byCases H: head.fst = v <;> try apply cast_eq
     assumption
 
--- SSAEnv
+theorem SSAScope.set_commutes ⦃v v': SSAVal⦄:
+    v' ≠ v →
+    ∀ ⦃scope: SSAScope δ⦄ ⦃τ τ' val val'⦄,
+    (set v τ val (set v' τ' val' scope)).equiv (set v' τ' val' (set v τ val scope)) := by
+  intros Hne scope τ τ' val val'
+  induction scope with
+  | nil =>
+    simp; simp [Hne, Hne.symm]
+    simp [equiv]; intros name τ''
+    byCases Hv: v' = name
+    byCases Hτ: τ' = τ'' <;> simp [Hne.symm]
+  | cons head tail Hind =>
+    simp [equiv] at *
+    intros name; specialize Hind name
+    simp at *
+    byCases Hv': head.fst = v' <;> simp [Hne]
+    byCases Hv: head.fst = v <;> simp [Hv']
+    byCases Hname: head.fst = name <;> assumption
+
+/-
+### SSAEnv
+-/
 
 inductive SSAEnv (δ: Dialect α σ ε) :=
   | One (scope: SSAScope δ)
@@ -170,13 +274,14 @@ def SSAEnv.getT {δ: Dialect α σ ε} (name: SSAVal):
   | One s => s.getT name
   | Cons s l => s.getT name <|> getT name l
 
-def SSAEnv.get {δ: Dialect α σ ε} (name: SSAVal) (τ: MLIRType δ):
-  SSAEnv δ → Option τ.eval
-  | One s => s.get name τ
-  | Cons s l => s.get name τ <|> get name τ l
-
-@[simp] def SSAEnv.get_One:
-  SSAEnv.get name τ (.One scope) = scope.get name τ := rfl
+def SSAEnv.get {δ: Dialect α σ ε} (name: SSAVal) (τ: MLIRType δ) (env: SSAEnv δ) : Option (τ.eval) :=
+  match env.getT name with
+  | none => none
+  | some ⟨τ', v'⟩ =>
+      if H': τ' = τ then
+        some (cast (by simp [H']) v')
+      else
+        none
 
 def SSAEnv.set {δ: Dialect α σ ε} (name: SSAVal) (τ: MLIRType δ) (v: τ.eval):
   SSAEnv δ → SSAEnv δ
@@ -197,11 +302,33 @@ instance {δ: Dialect α σ ε}: DecidableEq ((τ: MLIRType δ) × τ.eval) :=
 def SSAEnv.eqOn (l: List SSAVal) (env₁ env₂: SSAEnv δ): Bool :=
   l.all (fun v => env₁.getT v == env₂.getT v)
 
+-- Leibniz-ish equality
+def SSAEnv.equiv (env₁ env₂: SSAEnv δ): Prop :=
+  ∀ name τ, env₁.get name τ = env₂.get name τ
+
 -- SSAEnv theorems
 
-theorem SSAEnv.getT_set_ne (v v': SSAVal):
+theorem SSAEnv.equiv_rfl {δ: Dialect α σ ε} (env: SSAEnv δ):
+  env.equiv env := by intros v τ; rfl
+
+theorem SSAEnv.equiv_symm {δ: Dialect α σ ε} ⦃env env': SSAEnv δ⦄:
+    env.equiv env' → env'.equiv env := by
+  intros H name
+  specialize H name
+  simp [H]
+
+theorem SSAEnv.equiv_trans {δ: Dialect α σ ε}:
+    ∀ ⦃env₁ env₂: SSAEnv δ⦄, env₁.equiv env₂ →
+    ∀ ⦃env₃⦄, env₂.equiv env₃ →
+    env₁.equiv env₃ := by
+  intros _ _ H1 _ H2 name
+  specialize H1 name
+  specialize H2 name
+  simp [H1, H2]
+
+theorem SSAEnv.getT_set_ne ⦃v v': SSAVal⦄:
     v' ≠ v →
-    ∀ (env: SSAEnv δ) (τ: MLIRType δ) val,
+    ∀ ⦃env: SSAEnv δ⦄ ⦃τ: MLIRType δ⦄ ⦃val⦄,
     getT v (set v' τ val env) = getT v env := by
   intros Hne env τ val
   cases env with
@@ -224,33 +351,67 @@ theorem SSAEnv.getT_set_eq (env: SSAEnv δ) (v: SSAVal) (τ: MLIRType δ) val:
     simp [getT, set, HOrElse.hOrElse, OrElse.orElse, Option.orElse]
     simp [SSAScope.getT_set_eq]
 
-theorem SSAEnv.get_set_ne_val (v v': SSAVal):
+theorem SSAEnv.get_set_ne_val ⦃v v': SSAVal⦄:
     v' ≠ v →
-    ∀ (env: SSAEnv δ) (τ τ': MLIRType δ) val,
-    get v τ' (set v' τ val env) = get v τ' env := by
+    ∀ ⦃env: SSAEnv δ⦄ ⦃τ τ': MLIRType δ⦄ ⦃val⦄,
+    get v τ (set v' τ' val env) = get v τ env := by
   intros Hne env τ τ' val
-  cases env with
-  | One s =>
-    simp [get, set]
-    rw [SSAScope.get_set_ne_val]
-    assumption
-  | Cons head tail =>
-    simp [get, set, HOrElse.hOrElse, OrElse.orElse, Option.orElse]
-    rw [SSAScope.get_set_ne_val]
+  simp [get]
+  rw [SSAEnv.getT_set_ne]
+  assumption
+
+theorem SSAEnv.get_set_eq_val {δ: Dialect α σ ε} (τ τ': MLIRType δ)
+  (env: SSAEnv δ) (v: SSAVal) (val: τ'.eval):
+    get v τ (set v τ' val env) =
+      if H': τ' = τ then
+        some (cast (by simp [H']) val)
+      else
+        none := by
+  simp [get, getT_set_eq]
+
+theorem SSAEnv.get_set {δ: Dialect α σ ε} (τ τ': MLIRType δ)
+  (env: SSAEnv δ) (v v': SSAVal) (val: τ'.eval):
+    get v τ (set v' τ' val env) =
+      if v' = v then
+        if H: τ' = τ then
+          some (cast (by simp [H]) val)
+        else
+          none
+      else
+        get v τ env := by
+  byCases H: v' = v
+  . simp [get_set_eq_val]
+  . rw [get_set_ne_val]
     assumption
 
 theorem SSAEnv.get_set_eq (v: SSAVal) (env: SSAEnv δ) (τ: MLIRType δ) val:
     get v τ (set v τ val env) = some val := by
-  cases env with
-  | One s =>
-    simp [get, set]
-    rw [SSAScope.get_set_eq]
-  | Cons head tail =>
-    simp [get, set, HOrElse.hOrElse, OrElse.orElse, Option.orElse]
-    rw [SSAScope.get_set_eq]
+  simp [get, getT_set_eq]
+  rfl
 
+theorem SSAEnv.equiv_set {δ: Dialect α σ ε} ⦃env₁ env₂: SSAEnv δ⦄:
+    env₁.equiv env₂ →
+    ∀ ⦃name τ v⦄, (set name τ v env₁).equiv (set name τ v env₂) := by
+  intros HEnv name τ v name' τ'
+  byCases Hname: name = name'
+  . simp [get_set_eq_val]
+  . repeat rw [get_set_ne_val] <;> try assumption
+    apply HEnv
 
--- Interactions manipulating the environment
+theorem SSAEnv.set_commutes ⦃v v': SSAVal⦄:
+    v' ≠ v →
+    ∀ ⦃env: SSAEnv δ⦄ ⦃τ τ': MLIRType δ⦄ ⦃val val'⦄,
+    equiv (set v τ val (set v' τ' val' env)) (set v' τ' val' (set v τ val env)) := by
+  intros Hne env τ τ' val val' v₂ τ₂
+  repeat rw [get_set]
+  split
+  . subst v
+    simp [Hne]
+  . split <;> simp
+
+/-
+### Interactions manipulating the environment
+-/
 
 inductive SSAEnvE (δ: Dialect α σ ε): Type → Type where
   | Get: (τ: MLIRType δ) → [Inhabited τ.eval] → SSAVal → SSAEnvE δ τ.eval
@@ -345,3 +506,25 @@ theorem interpSSA'_bind {δ: Dialect α σ ε}
   interpSSA' (δ := δ) (Fitree.bind t k) s₁ =
   Fitree.bind (interpSSA' t s₁) (fun (x,s₂) => interpSSA' (k x) s₂) := by
   apply Fitree.interpState_bind
+
+
+macro "simp_ssaenv" : tactic =>
+  `(tactic| (repeat rw [SSAEnv.getT_set_eq]);
+            (repeat rw [SSAEnv.getT_set_ne (by assumption)]);
+            (repeat rw [SSAEnv.get_set_eq]);
+            (repeat rw [SSAEnv.get_set_eq_val]);
+            (repeat rw [SSAEnv.get_set_ne_val (by assumption)]))
+
+macro "simp_ssaenv" "at" Hname:ident : tactic =>
+  `(tactic| (repeat rw [SSAEnv.getT_set_eq] at $Hname:ident);
+            (repeat rw [SSAEnv.getT_set_ne (by assumption)] at $Hname:ident);
+            (repeat rw [SSAEnv.get_set_eq] at $Hname:ident);
+            (repeat rw [SSAEnv.get_set_eq_val] at $Hname:ident);
+            (repeat rw [SSAEnv.get_set_ne_val (by assumption)] at $Hname:ident))
+
+macro "simp_ssaenv" "at" "*" : tactic =>
+  `(tactic| (repeat rw [SSAEnv.getT_set_eq] at *);
+            (repeat rw [SSAEnv.getT_set_ne (by assumption)] at *);
+            (repeat rw [SSAEnv.get_set_eq] at *);
+            (repeat rw [SSAEnv.get_set_eq_val] at *);
+            (repeat rw [SSAEnv.get_set_ne_val (by assumption)] at *))
